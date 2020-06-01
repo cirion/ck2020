41b86ddc79d430e2ec3bd069a6d436dea82ca151
Chris King <chris@velosmobile.com>
2020-06-01 13:57:20 -0700

    Initial Hello World.

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+package com.seberin.bday
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8008) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
---
+++ test/ApplicationTest.kt
@@ -0,0 +1,32 @@
+package com.seberin.bday
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.webjars.*
+import java.time.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.logging.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
